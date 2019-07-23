b07ee01ade12cedc9219c9cf7a058bc54d7cf32d
KitchenLab3 <KitchenLab3@TEKCENTER.LOCAL>
2019-07-23 09:58:18 -0700

    updated first row

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>The New Breads</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+<h1>The breads</h1>
+        <ul class="nav nav-tabs">
+                <li class="nav-item">
+                  <a class="nav-link active" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">loonk</a>
+                </li>
+                <li class="nav-item">
+                   <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">lonk</a>
+                      </li>
+              </ul>
+
+<div class="row">
+<div class="col"><p> this is an ancient bread used for infinite life</p>
+<img width=300 src= "https://d2gk7xgygi98cy.cloudfront.net/1360-3-large.jpg" alt=""></div>
+<div class="col">
+<h3>What are the benifits of bread?</h3> 
+<p>Although it may not seem like it, bread can give a human godlike powers. including: Flying, Creating carbon, standing on clouds and the ability to create more bread </p>
+
+
+<p>you may ask. How do i recive these powers? simple, you have to eat twenty loaves of bread ibn one sitting. one bag of bagles is allowed if you are out of bread.</p>
+
+</div>
+
+</div>
+
+
+
+</div>
+
+
+
+
+    
+</body>
+</html>
